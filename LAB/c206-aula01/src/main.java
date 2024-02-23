5dd1e9fc585d64bb8131ff35d686790c9eb48b7e
Beatriz de Araújo Cardozo <102707560+beatriz-a-cardozo@users.noreply.github.com>
2024-02-23 08:13:06 -0300

    Update main.java
    Correção dos erros no código

--- LAB/c206-aula01/src/main.java
+++ LAB/c206-aula01/src/main.java
@@ -1,10 +1,13 @@
+// identifica como a classe - main
 public class main {
+    // função main
     public static void main(String[] args) {
+        // chama a função fatorial e mostra seu resultado
         System.out.println(fatorial(5));
-
+        // chama a função mostraPares
         mostraPares(23);
     }
-
+    // função que cácula o fatorial de um inteiro
     public static int fatorial (int n) {
         int res = n;
 
@@ -12,16 +15,17 @@ public class main {
             res = res * (n-1);
             n--;
         }
-        return n;
+        return res;
     }
-
+    // função que identifica o números pares entre 0 e um inteiro
     public static void mostraPares(int n) {
 
         while(n>0) {
-            if (n/2 == 0) {
+            if (n%2 == 0) {
                 System.out.println(n);
             }
             n--;
         }
 }
 }
+
